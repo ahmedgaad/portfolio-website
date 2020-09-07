c5fce1e25657500bcef6637b9e40636e33b3e55f
ahmed gad <agad92998@gmail.com>
2020-09-07 14:17:10 +0200

    adding my portfolio

---
+++ app.js
@@ -0,0 +1,10 @@
+<div id="c">
+<header class="head">
+    <nav id="ul">
+        <li><a href="#home">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#skills">Skills</a></li>
+        <li><a href="#contact">Contact</a></li>
+    </nav>
+</header>
+</div>
